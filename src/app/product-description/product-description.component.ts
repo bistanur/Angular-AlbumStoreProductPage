9681e2a53976dd6b486a0d53a1be92aea5b48099
Anurag Bist <AB1231@dhltd.corp>
2019-05-13 15:40:47 -0400

    fixed album import

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Subscriber } from 'rxjs/Subscriber';
-import { Album } from 'app/album';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
